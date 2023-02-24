86f7c8f35c0125214104a0355f068854cb81ed4d
Mikhail Delos Trinos <mikhaildelostrinos@Mikhails-MacBook-Air.local>
2023-02-24 14:51:32 +0800

    Apollo 0: initial commit with devise authentication

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :dashboard 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "dashboard#index"
+end
