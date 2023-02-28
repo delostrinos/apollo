c552a6006f980c3e2ee4586cc1f8345c6ebb783a
Mikhail Delos Trinos <mikhaildelostrinos@Mikhails-MacBook-Air.local>
2023-02-28 17:21:14 +0800

    Apollo 3: production mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "apollo_production"
 
   # config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'apollo-production.up.railway.app',
+    user_name:            ENV["EMAIL_USER"],
+    password:             ENV["EMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {host: ENV["APP_HOST"], port: 80}
 
   # Ignore bad email addresses and do not raise email delivery errors.
